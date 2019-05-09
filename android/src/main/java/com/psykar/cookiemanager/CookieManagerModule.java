bc2189a9f00768572a21f988f1198e9fdf06d1e1
wilson <wilson@waveo.com>
2019-05-10 00:02:49 +0800

    Added 'set' method for android

--- android/src/main/java/com/psykar/cookiemanager/CookieManagerModule.java
+++ android/src/main/java/com/psykar/cookiemanager/CookieManagerModule.java
@@ -12,13 +12,26 @@ import com.facebook.react.bridge.Promise;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.ParseException;
 import java.net.URI;
+import java.text.DateFormat;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class CookieManagerModule extends ReactContextBaseJavaModule {
+
+    private static final String COOKIE_HEADER = "Cookie";
+    private static final String VERSION_ZERO_HEADER = "Set-cookie";
+
+    private static final String COOKIE_DOMAIN_PROPERTY = "Domain";
+    private static final String COOKIE_PATH_PROPERTY = "Path";
+    private static final String COOKIE_EXPIRES_PROPERTY = "Expires";
+
+    private static final String ISO_DATE_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     private ForwardingCookieHandler cookieHandler;
 
@@ -33,7 +46,32 @@ public class CookieManagerModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void set(ReadableMap cookie, boolean useWebKit, final Promise promise) throws Exception {
-        throw new Exception("Cannot call on android, try setFromResponse");
+        try {
+            String cookieName = this.confirmCookieProperty( "name", cookie, true );
+            String cookieValue = this.confirmCookieProperty( "value", cookie, true );
+            String cookieOrigin = this.confirmCookieProperty( "origin", cookie, true );
+            String cookieDomain = this.confirmCookieProperty( "domain", cookie, false );
+            String cookiePath = this.confirmCookieProperty( "path", cookie, false );
+            String cookieVersion = this.confirmCookieProperty( "version", cookie, false );
+            String cookieExpiration = this.confirmCookieProperty( "expiration", cookie, false );
+
+            String cookieString = cookieName + '=' + cookieValue + ';';
+
+            if( !cookieDomain.isEmpty() ) {
+                cookieString += ' ' + COOKIE_DOMAIN_PROPERTY + '=' + cookieDomain + ';';
+            }
+            if( !cookiePath.isEmpty() ) {
+                cookieString += ' ' + COOKIE_PATH_PROPERTY + '=' + cookiePath + ';';
+            }
+            if( !cookieExpiration.isEmpty() ) {
+                cookieString += ' ' + COOKIE_EXPIRES_PROPERTY + '=' + this.convertToUTC( cookieExpiration ) + ';';
+            }
+
+            this.setCookie( cookieOrigin, cookieString );
+            promise.resolve( true );
+        } catch( Exception e ) {
+            promise.reject(e);
+        }
     }
 
     @ReactMethod
@@ -88,4 +126,32 @@ public class CookieManagerModule extends ReactContextBaseJavaModule {
             }
         });
     }
+
+    private void setCookie( String url, String value ) throws URISyntaxException, IOException {
+        URI uri = new URI(url);
+        Map<String, List<String>> cookieMap = new HashMap<>();
+        cookieMap.put( VERSION_ZERO_HEADER, Collections.singletonList(value) );
+        this.cookieHandler.put( uri, cookieMap );
+    }
+
+    private String confirmCookieProperty( String propName, ReadableMap cookieObj, Boolean isNecessary ) throws Exception {
+        String propValue = cookieObj.hasKey( propName ) ? cookieObj.getString( propName ) : "";
+
+        if( propValue != null && !propValue.isEmpty() ) {
+            return propValue;
+        } else {
+            if( isNecessary ) {
+                throw new Exception( propName + " property of cookie obj must have a value" );
+            } else {
+                return "";
+            }
+        }
+    }
+
+	private String convertToUTC(String timestamp) throws ParseException, Exception {
+		SimpleDateFormat parser = new SimpleDateFormat( ISO_DATE_FORMAT_PATTERN );
+		Date parsedDate = parser.parse(timestamp);
+		SimpleDateFormat utcFormatter = new SimpleDateFormat( "E, dd MMM YYYY HH:mm:ss" );
+		return utcFormatter.format(parsedDate) + " GMT";
+   }
 }
